befb4e43a5c81ae431716414a3492263563dbf66
Clement <clement.dimmi@hotmail.fr>
2019-05-14 16:38:10 +0200

    amelioration dashbord et changement dextend des templates user html.twig

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+use App\Controller\UserController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
