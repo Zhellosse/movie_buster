b512e543787ac017d864f944cc718ce7f6e7a4eb
Mélissa <mel-2904@homail.fr>
2019-05-14 10:43:45 +0200

    mise en page formulaire, supression du fichier (formulaire.html.twig) pour n'avoir que le base.html.twig

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -35,10 +35,10 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // do anything else you need here, like send an email
-
+            dd('$user');
             return $this->redirectToRoute('movie');
         }
-
+       
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
