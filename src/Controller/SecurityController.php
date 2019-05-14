47bea4fcfe6fb10f0bdd05e6262885b5d514498e
Maxime <maxime.coolombier@gmail.com>
2019-05-14 14:25:08 +0200

    définition des roles

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
+/**
+     * @Route("/login")
+     */
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
