f59b1ef668c638e1475fee07ffd7ca8d02330a22
Maxime <maxime.coolombier@gmail.com>
2019-05-21 14:40:26 +0200

    lala

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -25,7 +25,12 @@ class User implements UserInterface
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=180, unique=true)
+    * @ORM\Column(type="string", length=180, unique=true)
+    * @Assert\NotBlank
+    * @Assert\Email(
+    *     message = "The email '{{ value }}' is not a valid email.",
+    *     checkMX = true
+    * )
      */
     private $email;
 
